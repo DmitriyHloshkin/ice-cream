d54cdbd60f172772226f46fe5bb4f74bb4094145
Dmitriy Hloshkin <dmitriyhloshkin@gmail.com>
2022-08-29 13:36:29 +0300

    add section benefit, adapt it, add slick slider  for mobile version

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,39 @@
-$(document).ready(function(){
+let slickSlider = $('.benefit__wrap');
 
-// burger menu
-$('.burger').on('click', function () {
-    $('.promo-panel').toggleClass('promo-panel_active');
-    $('.burger__elem').toggleClass('burger__elem_active');
-});
+$(document).ready(function () {
+
+    // burger menu
+    $('.burger').on('click', function () {
+        $('.promo-panel').toggleClass('promo-panel_active');
+        $('.burger__elem').toggleClass('burger__elem_active');
+    });
+
+    //slick-slider
+    if (isMobile()) {
+        initializedSlider();
+    }
+
+    $(window).resize(function () {
+        if (isMobile()) {
+            initializedSlider();
+        } else {
+            if (slickSlider.hasClass('slick-initialized')) {
+                slickSlider.slick('unslick');
+            }
+
+        }
+    });
+
+    function isMobile() {
+        return $(window).width() <= 576;
+    }
+
+    function initializedSlider() {
+        slickSlider.slick({
+            prevArrow: '<button class="arrows arrows_prev"><img src="icons/prev-arrow.svg" alt="arrow"></button>',
+            nextArrow: '<button class="arrows arrows_next"><img src="icons/next-arrow.svg" alt="arrow"></button>',
+            autoplay: true
+        });
+    }
 
 });
