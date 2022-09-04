bf3acf48a9d88a5497abda04e18d75a187e50425
Dmitriy Hloshkin <dmitriyhloshkin@gmail.com>
2022-09-04 10:55:21 +0300

    site optimization, add production version, gulp optimization

--- gulpfile.js
+++ gulpfile.js
@@ -1,52 +1,130 @@
 // Modules
 const gulp = require('gulp');
+const del = require('del');
 const browserSync = require('browser-sync');
+const htmlmin = require('gulp-htmlmin');
 const sass = require('gulp-sass')(require('sass'));
+const rename = require("gulp-rename");
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
-const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
 
-// files src
-const cssStyle = './src/css/**/*.css';
-const indexFile = './src/*.html';
-const sassStyle = './src/sass/**/*.+(scss|sass)';
+//Path
+const path = {
+  build: {
+    html: "./dist",
+    css: "./dist/css",
+    js: "./dist/js",
+    fonts: "./dist/fonts",
+    icons: "./dist/icons",
+    image: "./dist/image"
+  },
+  src: {
+    html: "./src/*.html",
+    scss: "./src/+(sass|scss)/*.+(scss|sass)",
+    js: "./src/js/**/*.js",
+    fonts: "./src/fonts/**/*",
+    icons: "./src/icons/**/*",
+    image: "./src/image/**/*"
+  },
+  watch: {
+    html: "./src/**/*.html",
+    scss: "./src/sass/**/*.+(scss|sass|css)",
+    js: "./src/js/**/*.js",
+    fonts: "./src/fonts/**/*",
+    icons: "./src/icons/**/*",
+    image: "./src/image/**/*"
+  },
+  buildFolder: "./dist",
+  srcFolder: "./src",
+}
 
-// browserSync server
-gulp.task('startServer', function () {
-    browserSync.init({
-        server: {
-            baseDir: "./src"
-        }
-    });
-
-    gulp.watch(cssStyle).on('change', browserSync.reload);
-    gulp.watch(indexFile).on('change', browserSync.reload);
-});
-
-// Compilate scss|sass in css
-gulp.task('compileSass', function () {
-    return gulp.src(sassStyle)
-        .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(rename({
-            dirname: "",
-            basename: "style",
-            prefix: "",
-            suffix: ".min",
-            extname: ".css"
-        }))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(cleanCSS({ compatibility: 'ie8' }))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
-
-//  Auto compile sass to css
-gulp.task("watchSass", function () {
-    gulp.watch(sassStyle, gulp.parallel("compileSass"))
+//Tasks
+gulp.task("removeAll", (done) => {
+  del.sync(path.buildFolder);
+  done();
 })
 
-//  Start Gulp
-gulp.task("default", gulp.parallel("startServer", "watchSass"));
+gulp.task("serverOn", () => {
+  browserSync.init({
+    server: {
+      baseDir: path.buildFolder
+    },
+    notify: true,
+    port: 3000
+  });
+})
 
+gulp.task("serverReload", (done) => {
+  browserSync.reload();
+  done();
+});
+
+gulp.task("copyHtml", () => {
+  return gulp.src(path.src.html)
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest(path.build.html))
+    .pipe(browserSync.stream());
+})
+
+gulp.task("copyCss", () => {
+  return gulp.src(path.src.scss)
+    .pipe(sass.sync({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({
+      dirname: "",
+      basename: "style",
+      prefix: "",
+      suffix: ".min",
+      extname: ".css"
+    }))
+    .pipe(autoprefixer({
+      cascade: false
+    }))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+
+    .pipe(gulp.dest(path.build.css))
+    .pipe(browserSync.stream());
+})
+
+gulp.task("copyScripts", () => {
+  return gulp.src(path.src.js)
+    .pipe(gulp.dest(path.build.js))
+    .pipe(browserSync.stream());
+})
+
+gulp.task("copyFonts", () => {
+  return gulp.src(path.src.fonts)
+    .pipe(gulp.dest(path.build.fonts))
+    .pipe(browserSync.stream());
+})
+
+gulp.task("copyIcons", () => {
+  return gulp.src(path.src.icons)
+    .pipe(gulp.dest(path.build.icons))
+    .pipe(browserSync.stream());
+})
+
+gulp.task("copyImage", () => {
+  return gulp.src(path.src.image)
+    .pipe(imagemin())
+    .pipe(gulp.dest(path.build.image))
+    .pipe(browserSync.stream());
+})
+
+
+//main scripts gulp
+const main = gulp.series("removeAll", "copyHtml", "copyCss", "copyScripts", "copyFonts", "copyIcons", "copyImage");
+const dev = gulp.series(main, gulp.parallel(watcher, "serverOn"));
+
+
+gulp.task('default', dev);
+
+// watcher
+function watcher() {
+  gulp.watch(path.watch.html, gulp.series("copyHtml"));
+  gulp.watch(path.watch.scss, gulp.series("copyCss"));
+  gulp.watch(path.watch.js, gulp.series("copyScripts"));
+  gulp.watch(path.watch.fonts, gulp.series("copyFonts"));
+  gulp.watch(path.watch.icons, gulp.series("copyIcons"));
+  gulp.watch(path.watch.image, gulp.series("copyImage"));
+}
